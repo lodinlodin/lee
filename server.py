b945c1c7ce37dd757d678e42030eba8bb76319c5
lll <mamagloria8@gmail.com>
2019-11-21 21:46:38 +0800

    switch to gunicorn

--- server.py
+++ server.py
@@ -42,7 +42,3 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-
-
-if __name__ == "__main__":
-    app.run(port=os.environ.get('PORT'))
