5e8ba93f4381f9e2a07d51dcb45b462d9ded135d
lll <mamagloria8@gmail.com>
2019-11-22 23:13:45 +0800

    3

--- server.py
+++ server.py
@@ -38,27 +38,7 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    foo == event.message.text
-        if foo[0] == '?':
-            pass
-            print('WTF')
-            
+def handle_message(event):   
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-
-    message = TextSendMessage(text='Hello, world')
-    line_bot_api.reply_message(event.reply_token, message)
-
-    message = ImageSendMessage(
-    original_content_url='https://example.com/original.jpg',
-    preview_image_url='https://example.com/preview.jpg'
-    )
-    line_bot_api.reply_message(event.reply_token, message)
-
-    message = StickerSendMessage(
-    package_id='1',
-    sticker_id='1'
-    )
-    line_bot_api.reply_message(event.reply_token, message)
